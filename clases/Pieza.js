29843190c05300506b9677bfdaf6772a23d6b4bd
JAIME FERNANDEZ ORTEGA <jaimefo1@gmail.com>
2023-02-05 19:42:42 +0100

    05/02/2023 creo que me voy a pasar a javascript...

---
+++ clases/Pieza.js
@@ -0,0 +1,12 @@
+class Pieza {
+
+    constructor(numero, color, aristas) {
+        this.numero = numero;
+        this.color = color;
+        this.aristas = aristas;
+        this.posicionActual = 1;
+    }
+
+}
+
+
